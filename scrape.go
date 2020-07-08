c5b4eb0b0ecca7f11e57b6bb20c53576469d9dda
siko408 <gamerbloxhd@gmail.com>
2020-07-07 21:45:11 -0700

    Serialized result

--- scrape.go
+++ scrape.go
@@ -4,21 +4,43 @@ import (
 	"fmt"
 
 	"github.com/gocolly/colly"
+	 "encoding/json"
+	 "io"
+   "log"
+   "os"
 )
+
+type webData struct {
+	Body string
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
 func main() {
+
 	// Instantiate default collector
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
-
+	c.OnHTML("body", func(e *colly.HTMLElement) {
+                //bodyData := e.Attr("body")
+				fmt.Println(e.Text)
+				jsonStruct := &webData{Body:e.Text}
 		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+                //fmt.Printf("Text found: %q -> %s\n", e.Text, link)
+			result, err := json.Marshal(jsonStruct)
+			   if err != nil {
+				   fmt.Println(err)
+				   return
+			   }
+
+			  // fmt.Println(result)
+			   err2 := WriteToFile("result.txt", string(result))
+		   if err2 != nil {
+	   		log.Fatal(err2)
+   		}
 	})
+
 
 	// Before making a request print "Visiting ..."
 	c.OnRequest(func(r *colly.Request) {
@@ -27,4 +49,20 @@ func main() {
 
 	// Start scraping on https://hackerspaces.org
 	c.Visit("https://hackerspaces.org/")
+
+
+}
+
+func WriteToFile(filename string, data string) error {
+    file, err := os.Create(filename) ///
+    if err != nil {
+        return err
+    }
+    defer file.Close()
+
+    _, err = io.WriteString(file, data)
+    if err != nil {
+        return err
+    }
+    return file.Sync()
 }
